f7251ea242d32e9307a40f11da00e1221340a7ac
Mohammed-Alama <mohammedalsayed9688@gmail.com>
2019-04-26 05:32:12 +0200

    This is Simple ToDo App with using api from Json placholder

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+function About(){
+    return (
+     <React.Fragment>
+       <h1>About</h1>
+       <p>This is the TodoList v1.0.0 Powered by React  </p>
+     </React.Fragment>
+    )
+}
+
+
+export default About
